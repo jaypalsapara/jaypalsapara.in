b793be936eda87c554cba114fd52104ae261a080
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2025-10-19 15:28:49 +0530

    Add sitemap genarator

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://jaypalsapara.in',
+      // dynamicRoutes: ['/about'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
