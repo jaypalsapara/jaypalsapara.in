1bdfb7c318dea636b3714ec61f368b723c66d6b6
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2025-10-29 19:21:38 +0530

    Add profile images of reviewers

--- src/sections/testimonial.tsx
+++ src/sections/testimonial.tsx
@@ -14,7 +14,7 @@ const Testimonial = () => {
         description="What clients say about my work, and I would be truly happy to see you here, exploring the quality and dedication I bring to every project."
       />
       <SectionContent className="mb-14 lg:mb-18">
-        <Wrapper className="gap-y-4">
+        <Wrapper className="gap-y-6 lg:gap-x-6">
           {Testimonials.map((data, index) => (
             <div key={`testimonial-${index}`} className="col-span-4 flex flex-col gap-4 rounded-xs border p-4 shadow-xs">
               <div className="flex">
@@ -29,9 +29,11 @@ const Testimonial = () => {
               <div className="mt-auto flex items-center justify-between">
                 <div className="grid">
                   <small className="font-medium">{data.user.name}</small>
-                  <small>{data.type}</small>
+                  <small className="text-muted-foreground">{data.type}</small>
                 </div>
-                <picture className="grid size-11 place-content-center rounded-xs bg-muted text-muted-foreground">{data.user.sort_name}</picture>
+                <picture className="grid size-11 place-content-center overflow-hidden rounded-xs bg-muted text-muted-foreground">
+                  {data.user.avatar ? <img src={data.user.avatar} alt={data.user.avatar} className="inset-0 object-cover" /> : data.user.sort_name}
+                </picture>
               </div>
             </div>
           ))}
