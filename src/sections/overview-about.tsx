f67a8f78185e426ec13427e8e30e25f394547825
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2025-11-12 18:40:51 +0530

    Update overview-about.tsx

--- src/sections/overview-about.tsx
+++ src/sections/overview-about.tsx
@@ -1,36 +1,27 @@
+import SectionBadge from '@/components/section-badge';
+import SectionContent from '@/components/section-content';
+import SectionHeader from '@/components/sectoin-header';
 import Wrapper from '@/components/wrapper';
-import Overview from '@/data/overview-about-me.json';
-import { Award, Languages, MapPin } from 'lucide-react';
-const Icons = {
-  Award,
-  Languages,
-  MapPin,
-} as const;
-
-type IconName = keyof typeof Icons;
+import { Info } from 'lucide-react';
 
 const OverviewAbout = () => {
   return (
-    <Wrapper className="gap-x-0 px-0 lg:gap-x-0 lg:px-0">
-      {Overview.map((data) => {
-        const IconName = data.icon as IconName;
-        const Icon = Icons[IconName];
-        return (
-          <div
-            className="pad-x col-span-4 flex flex-col gap-x-5 py-8 not-first:border-t lg:border-e lg:py-12 lg:not-first:border-t-0 lg:nth-[3n]:border-e-0 lg:nth-last-[n+4]:border-b"
-            key={`service-${data.name}`}
-          >
-            <div className="grid size-12 place-content-center rounded-xs bg-accent-100/60 dark:bg-accent-900/28">
-              <Icon className="size-6 stroke-accent" />
-            </div>
-            <div className="mt-4 grid">
-              <h6>{data.name}</h6>
-              <small className="mt-1 text-pretty text-muted-foreground">{data.description}</small>
-            </div>
-          </div>
-        );
-      })}
-    </Wrapper>
+    <>
+      <SectionBadge Icon={Info} title="Information" />
+      <SectionHeader title="Know more" description="A few details that define me and my journey and get to know me a little better through this snapshot." />
+      <SectionContent className="mt-6 mb-14 lg:mt-18 lg:mb-18">
+        <Wrapper className="gap-x-0 px-0 lg:gap-x-0 lg:px-0">
+          <p className="pad-x col-span-8">
+            Hi, I’m Jaypal Sapara! I pursued a <strong className="font-medium">Bachelor’s degree in Computer Applications</strong> (In 2020) and{' '}
+            <strong className="font-medium">I absolutely love coding</strong> — it’s both my passion and my creative outlet. Outside of coding, I enjoy{' '}
+            <strong className="font-medium">gaming, drawing, and dancing,</strong> which help me stay happy. <br /> <br /> I’m based in{' '}
+            <strong className="font-medium">Surendranagar, Gujarat, India,</strong> and I’m comfortable communicating in{' '}
+            <strong className="font-medium">English, Hindi, and Gujarati.</strong> I’m always excited to learn, create, and explore new ideas through
+            technology.
+          </p>
+        </Wrapper>
+      </SectionContent>
+    </>
   );
 };
 
