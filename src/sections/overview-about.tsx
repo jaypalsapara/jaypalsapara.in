2f268aab2348546e9b8876c582fb0fa9f76ff44b
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2025-11-12 18:43:25 +0530

    Update overview-about.tsx

--- src/sections/overview-about.tsx
+++ src/sections/overview-about.tsx
@@ -11,7 +11,7 @@ const OverviewAbout = () => {
       <SectionHeader title="Know more" description="A few details that define me and my journey and get to know me a little better through this snapshot." />
       <SectionContent className="mt-6 mb-14 lg:mt-18 lg:mb-18">
         <Wrapper className="gap-x-0 px-0 lg:gap-x-0 lg:px-0">
-          <p className="pad-x col-span-8">
+          <p className="pad-x col-span-8 text-pretty">
             Hi, I’m Jaypal Sapara! I pursued a <strong className="font-medium">Bachelor’s degree in Computer Applications</strong> (In 2020) and{' '}
             <strong className="font-medium">I absolutely love coding</strong> — it’s both my passion and my creative outlet. Outside of coding, I enjoy{' '}
             <strong className="font-medium">gaming, drawing, and dancing,</strong> which help me stay happy. <br /> <br /> I’m based in{' '}
