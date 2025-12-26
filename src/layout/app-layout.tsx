0395a442981e2fb4506da1cf9e80cda75f3adc68
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2025-12-26 12:17:39 +0530

    Update SEO

--- src/layout/app-layout.tsx
+++ src/layout/app-layout.tsx
@@ -1,5 +1,6 @@
 import NavBar from '@/components/nav-bar';
 import OverflowLine from '@/components/overflow-line';
+import { APP_URL } from '@/constants/site';
 import Footer from '@/sections/footer';
 import { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router';
@@ -15,6 +16,24 @@ const AppLayout = () => {
     });
     // Hide mobile popover
     mobileNav?.hidePopover();
+
+    /**
+     * Canonical
+     */
+    let canonicalLink = document.querySelector("link[rel='canonical']");
+
+    if (!canonicalLink) {
+      canonicalLink = document.createElement('link');
+      canonicalLink.setAttribute('rel', 'canonical');
+      document.head.appendChild(canonicalLink);
+    }
+
+    canonicalLink.setAttribute('href', APP_URL + pathname);
+
+    /**
+     * Clean up
+     */
+    return () => {};
   }, [pathname]);
 
   return (
