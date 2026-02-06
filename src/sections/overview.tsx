5c3b8c00a0ad7a4a8c60aadece3c8ee63c9de668
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-02-06 13:03:54 +0530

    Add new overview

--- src/sections/overview.tsx
+++ src/sections/overview.tsx
@@ -12,22 +12,22 @@ import overviewData from '@/data/overview.json';
 
 const Overview = () => {
   return (
-    <Wrapper className="gap-x-0 px-0 lg:gap-x-0 lg:px-0">
-      <div className="pad-x col-span-4 flex items-center gap-x-5 py-8 not-last:border-b lg:col-span-3 lg:py-11 lg:not-last:border-e lg:not-last:border-b-0">
-        <p className="font-medium text-muted-foreground">Proof by numbers you can trust, reflecting delivered and client satisfaction.</p>
+    <Wrapper className="grid-cols-2 gap-x-0 px-0 lg:grid-cols-5 lg:gap-x-0 lg:px-0">
+      <div className="pad-x col-span-2 flex items-center gap-x-5 py-8 not-last:border-b lg:col-span-1 lg:py-9 lg:not-last:border-e lg:not-last:border-b-0">
+        <p className="font-medium text-muted-foreground">Proof by numbers you can trust</p>
       </div>
       {overviewData.map((data) => {
         // const IconName = data.icon as IconName;
         // const Icon = Icons[IconName];
         return (
           <div
-            className="pad-x col-span-4 flex items-center justify-center gap-x-5 py-8 not-last:border-b lg:col-span-3 lg:py-11 lg:not-last:border-e lg:not-last:border-b-0"
+            className="flex items-center justify-center gap-x-5 px-4 py-8 not-nth-last-[-n+2]:border-b max-lg:not-even:border-l lg:py-10 lg:not-last:border-e lg:not-last:border-b-0"
             key={`overview-${data.label}`}
           >
             {/* <Icon className="size-11 stroke-accent stroke-1 lg:size-11.5" /> */}
             <div>
               <h3 className="text-center font-medium">{data.label}</h3>
-              <p className="mt-2 text-muted-foreground">{data.subtitle}</p>
+              <p className="mt-1 text-center text-pretty text-muted-foreground">{data.subtitle}</p>
             </div>
           </div>
         );
