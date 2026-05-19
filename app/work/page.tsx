080257442950ae95fbb19292024af8cc33eb9817
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-05-19 11:04:42 +0530

    Add idea-text component

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,20 +1,8 @@
-'use client';
-
 import Footer from '@/components/footer';
 import H1 from '@/components/h1';
-import { motion, useScroll, useTransform } from 'motion/react';
-import Image from 'next/image';
+import { IdeaText } from '@/components/idea-text';
 
 export default function Work() {
-  const { scrollY } = useScroll();
-  const { foregroundY } = useTransform(
-    scrollY,
-    [0, 1],
-    {
-      foregroundY: [1, 0.65],
-    },
-    { clamp: false },
-  );
   return (
     <>
       <main className="flex w-full flex-1 flex-col relative">
@@ -23,18 +11,7 @@ export default function Work() {
             <H1 className="font-bold">
               <span className="text-muted-foreground/50">I</span> develop{' '}
               <span className="text-muted-foreground/50">and</span> deploy{' '}
-              <span className="text-muted-foreground/50">web and experience from</span>{' '}
-              <div className="inline-flex relative">
-                idea
-                <motion.div
-                  className="absolute -top-6 lg:-top-8 xl:-top-9"
-                  style={{
-                    y: foregroundY,
-                  }}
-                >
-                  <Image src={'/images/cloud.png'} width={256} height={256} alt="Cloud" className="drop-shadow-xl" />
-                </motion.div>
-              </div>{' '}
+              <span className="text-muted-foreground/50">web and experience from</span> <IdeaText />{' '}
               <span className="text-muted-foreground/50 me-1">to</span>
               <div className="inline-flex bg-yellow-300 px-6 items-center justify-center rounded-full">
                 <span>final_fixed</span>
---
+++ components/idea-text.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { motion, useScroll, useTransform } from 'motion/react';
+import Image from 'next/image';
+
+export const IdeaText = () => {
+  const { scrollY } = useScroll();
+  const { cloudY } = useTransform(
+    scrollY,
+    [0, 1],
+    {
+      cloudY: [1, 0.65],
+    },
+    { clamp: false },
+  );
+  return (
+    <div className="inline-flex relative">
+      idea
+      <motion.div
+        className="absolute -top-6 lg:-top-8 xl:-top-9"
+        style={{
+          y: cloudY,
+        }}
+      >
+        <Image src={'/images/cloud.png'} width={256} height={256} alt="Cloud" className="drop-shadow-xl" />
+      </motion.div>
+    </div>
+  );
+};
