ee1d23001f82e4c26a2b87190a9a5fc5f547f454
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-06-06 11:32:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+'use client';
+
 import Footer from '@/components/footer';
 import H1 from '@/components/h1';
 import TransitionLink from '@/components/transition-link';
 import { Button } from '@/components/ui/button';
 import { ArrowRight } from 'lucide-react';
+import { motion } from 'motion/react';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <main className="flex w-full flex-1 flex-col relative isolate">
+      <motion.main
+        className="flex w-full flex-1 flex-col relative isolate"
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+          transition: { ease: [0.39, 0.575, 0.565, 1.0], delay: 0.4 },
+        }}
+      >
         <div className="grid xl:grid-cols-2 py-8 px-4 absolute w-full">
           <div className="xl:col-start-2">
             <H1 className="font-bold max-w-[16ch]">Builds result-driven web applications</H1>
@@ -36,7 +48,7 @@ export default function Home() {
           priority
           className="-z-1 min-h-210 object-cover w-full block md:hidden"
         />
-      </main>
+      </motion.main>
       <Footer navigation={{ name: 'About', path: '/about' }} />
     </>
   );
