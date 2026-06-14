f5c381cb6b2818cc0a28ad906497599cf9f9dd31
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-06-14 12:06:52 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,10 @@
 import Database from 'better-sqlite3';
 import { drizzle } from 'drizzle-orm/better-sqlite3';
+import path from 'path';
 import * as schema from './schema';
 
-const sqlite = new Database('./database/app.db');
+const dbPath = path.join(process.cwd(), 'database', 'app.db');
+
+const sqlite = new Database(dbPath);
 
 export const db = drizzle(sqlite, { schema });
