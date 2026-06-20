9a0a0a4fad3333ee028dd5c34845a9b84895f5dc
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-06-20 10:11:05 +0530

    fix: auto reset to top

--- components/view-transition-shell.tsx
+++ components/view-transition-shell.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import { ViewTransitions } from 'next-view-transitions';
+import { usePathname } from 'next/navigation';
+import { useLayoutEffect } from 'react';
 
 export default function ViewTransitionShell({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]);
+
   return <ViewTransitions>{children}</ViewTransitions>;
 }
