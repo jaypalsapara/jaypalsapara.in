8f2793abd4b06ce7045268cca10063a73b604b2c
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-06-28 11:23:11 +0530

    De-seo some section

--- components/project-square.tsx
+++ components/project-square.tsx
@@ -11,7 +11,7 @@ const keyAsLabel = {
 
 export default function ProjectSquare({ data }: { data: ProjectProps }) {
   return (
-    <div className="group relative">
+    <div className="group relative" data-nosnippet>
       <div className="w-full aspect-square overflow-hidden rounded-lg lg:rounded-xl pile isolate">
         <Image
           src={`/images/projects/${data.slug}/${data.thumbnail}`}
