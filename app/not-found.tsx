628f2454455bb9ce4424f81bd85fc8c757da11ef
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-07-06 09:52:20 +0530

    Update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,30 @@
+import H1 from '@/components/h1';
+import P from '@/components/p';
+import TransitionLink from '@/components/transition-link';
+import { Button } from '@/components/ui/button';
+import { ArrowLeft } from 'lucide-react';
+
 export default function NotFound() {
-  return <div>404</div>;
+  return (
+    <>
+      <main className="flex w-full flex-1 flex-col relative">
+        <section className="grid lg:grid-cols-2 pt-8 pb-6 lg:pb-8 px-4 w-full">
+          <div className="lg:col-start-2">
+            <H1 className="font-bold">
+              404 <span className="text-muted-foreground/50">Page not found</span>
+            </H1>
+            <P className="mt-10 max-w-[40ch] text-muted-foreground">
+              This page may have been moved or deleted. Return home to continue browsing.
+            </P>
+            <Button className="w-44 h-10 mt-9 rounded-full" asChild>
+              <TransitionLink href={'/'}>
+                <span className="sr-only">Home</span>
+                <ArrowLeft className="size-6" strokeLinejoin="miter" strokeLinecap="square" />
+              </TransitionLink>
+            </Button>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
